eea01325f4ef10f95d2066ed78d30bacd6967acb
David Turner <git@dwt27.co.uk>
2021-12-21 11:49:25 +0000

    Setup I2C, simple DS3231 demo
    * Add I2C initialisation to board_setup
    * Setup DS3231 driver
    * Basic demo reading time from the DS3231 and rendering to the display

--- smalltime-rust/src/board.rs
+++ smalltime-rust/src/board.rs
@@ -1,8 +1,14 @@
 use crate::display;
 use crate::hal;
-use hal::{prelude::*, stm32};
+use hal::{prelude::*, stm32, i2c::I2c};
+use embedded_hal::blocking::i2c;
+use stm32f0xx_hal::i2c::Error;
 
-pub fn board_setup() -> (display::DisplayPins, hal::delay::Delay) {
+pub fn board_setup() ->
+    (display::DisplayPins,
+     hal::delay::Delay,
+     impl i2c::Write<Error = Error> + i2c::WriteRead<Error = Error>)
+{
     let mut p = stm32::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
 
@@ -43,5 +49,12 @@ pub fn board_setup() -> (display::DisplayPins, hal::delay::Delay) {
         }
     });
 
-    (display_pins, delay)
+    // Configure I2C bus for DS3231
+    let i2c = cortex_m::interrupt::free(|cs| {
+        let scl = gpiob.pb6.into_alternate_af1(cs);
+        let sda = gpiob.pb7.into_alternate_af1(cs);
+        I2c::i2c1(p.I2C1, (scl, sda), 100.khz(), &mut rcc)
+    });
+
+    (display_pins, delay, i2c)
 }
--- smalltime-rust/src/main.rs
+++ smalltime-rust/src/main.rs
@@ -10,19 +10,46 @@ mod display;
 mod light;
 
 use stm32f0xx_hal as hal;
-
 use crate::hal::prelude::*;
+use ds323x::{Ds323x, Rtcc};
+
+// Take an Hours struct which may or may not be in 24-hour time and convert it
+// into 24 hour time.
+fn hours_to_24(hours: ds323x::Hours) -> u8 {
+    match hours {
+        ds323x::Hours::AM(x) => x,
+        ds323x::Hours::PM(x) => x + 12,
+        ds323x::Hours::H24(x) => x,
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (display_pins, mut delay) = board::board_setup();
+    let (display_pins, mut delay, i2c) = board::board_setup();
 
-    let mut my_display = display::Display::new(display_pins);
+    let mut disp = display::Display::new(display_pins);
 
-    loop {
-        for i in 0..10 {
-            my_display.display_num(i, 0);
-            delay.delay_ms(1_000_u32);
+    let mut rtc = Ds323x::new_ds3231(i2c);
+
+    loop { // Loop roughly every 1 second
+        let hours = rtc.get_hours().unwrap();
+        let minutes = rtc.get_minutes().unwrap();
+        let seconds = rtc.get_seconds().unwrap();
+
+        for _ in 0..166 {
+            disp.display_num(hours_to_24(hours) / 10, 0);
+            delay.delay_ms(1u32);
+            disp.display_num(hours_to_24(hours) % 10, 1);
+            delay.delay_ms(1u32);
+            disp.display_num(minutes / 10, 2);
+            delay.delay_ms(1u32);
+            disp.display_num(minutes % 10, 3);
+            delay.delay_ms(1u32);
+
+            disp.display_num(seconds / 10, 6);
+            delay.delay_ms(1u32);
+            disp.display_num(seconds % 10, 7);
+            delay.delay_ms(1u32);
         }
     }
 }
